3db87f9b9b6842b199ac719d55ef7824a437727d
Darren Taolin <darren.taolin8c@gmail.com>
2026-05-10 02:31:36 +0700

    initial

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+
+if(menuBtn){
+
+    menuBtn.addEventListener("click", ()=>{
+
+        navLinks.classList.toggle("active");
+
+    });
+
+}
